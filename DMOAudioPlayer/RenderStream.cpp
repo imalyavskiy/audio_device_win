c2756fa2139dc7f52c5f8124a7f9acc7bca956b1
Ilya Malyavskiy <ilya.malyavskiy@gmail.com>
2018-03-21 15:24:22 +0700

    MyAudioSourceImpl reads wav file with single WAV RIFF that inself turn contains just 'fmt ' and 'data' chunks.

--- DMOAudioPlayer/RenderStream.cpp
+++ DMOAudioPlayer/RenderStream.cpp
@@ -42,6 +42,11 @@ const IID IID_IAudioRenderClient
 #define WAVE_FORMAT_PCM         0x0001
 #endif
 
+const uint32_t riff_4cc = MAKEFOURCC('R', 'I', 'F', 'F');
+const uint32_t wave_4cc = MAKEFOURCC('W', 'A', 'V', 'E');
+const uint32_t fmt_4cc  = MAKEFOURCC('f', 'm', 't', ' ');
+const uint32_t data_4cc = MAKEFOURCC('d', 'a', 't', 'a');
+
 HRESULT PlayAudioStream(MyAudioSource *pMySource)
 {
     HRESULT hr;
@@ -196,68 +201,129 @@ bool MyAudioSourceImpl::Init(const std::string& file)
 
     m_file_size = end - begin;
 
-    DWORD dwRIFF = 0;
-    m_source_data.read(reinterpret_cast<char*>(&dwRIFF), sizeof(DWORD));
-    assert(MAKEFOURCC('R', 'I', 'F', 'F', ) == dwRIFF);
 
-    DWORD dwRIFFSize = 0;
-    m_source_data.read(reinterpret_cast<char*>(&dwRIFFSize), sizeof(DWORD));
-    assert(dwRIFFSize == end - m_source_data.tellg());
-
-    DWORD dwWAVE = 0;
-    m_source_data.read(reinterpret_cast<char*>(&dwWAVE), sizeof(DWORD));
-    assert(MAKEFOURCC('W', 'A', 'V', 'E', ) == dwWAVE);
-
-    DWORD dwChunkId = 0;
-    m_source_data.read(reinterpret_cast<char*>(&dwChunkId), sizeof(DWORD));
-    if (MAKEFOURCC('f', 'm', 't', ' ', ) == dwChunkId)
+    ChunkDescriptor riff_descriptor{ 0 };
+    while(m_source_data.rdstate() != std::ios_base::eofbit )
     {
-        for (;;) {
-            DWORD   chkSize = 0;
-            m_source_data.read(reinterpret_cast<char*>(&chkSize), sizeof(DWORD));
-            assert(chkSize == 16 || chkSize == 18 || chkSize == 40);
-
-            WORD    wFormatTag = 0; // 2(2)
-            m_source_data.read(reinterpret_cast<char*>(&wFormatTag), sizeof(WORD));
-
-            WORD    nChannels = 0;  // 2(4)
-            m_source_data.read(reinterpret_cast<char*>(&nChannels), sizeof(WORD));
-
-            DWORD   nSamplesPerSec = 0; // 4(8)
-            m_source_data.read(reinterpret_cast<char*>(&nSamplesPerSec), sizeof(DWORD));
-
-            DWORD   nAvgBytesPerSec = 0; // 4(12)
-            m_source_data.read(reinterpret_cast<char*>(&nAvgBytesPerSec), sizeof(DWORD));
-
-            WORD    nBlockAlign = 0; // 2(14)
-            m_source_data.read(reinterpret_cast<char*>(&nBlockAlign), sizeof(WORD));
-
-            WORD    wBitsPerSample = 0; // 2(16)
-            m_source_data.read(reinterpret_cast<char*>(&wBitsPerSample), sizeof(WORD));
-
-            if (chkSize == 16) break;
-
-            WORD    cbSize = 0; // 2(18)
-            m_source_data.read(reinterpret_cast<char*>(&dwRIFFSize), sizeof(WORD));
-
-            if (chkSize == 18) break;
-
-            WORD    wValidBitsPerSample = 0; // 2(20)
-            m_source_data.read(reinterpret_cast<char*>(&cbSize), sizeof(WORD));
-
-            DWORD   dwChannelMask = 0;	// 4(24)
-            m_source_data.read(reinterpret_cast<char*>(&dwChannelMask), sizeof(DWORD));
-
-            GUID    SubFormat{ 0 };	// 16(40)
-            m_source_data.read(reinterpret_cast<char*>(&SubFormat), sizeof(GUID));
-
-            assert(chkSize == 40);
-            
+        m_source_data.read(reinterpret_cast<char*>(&riff_descriptor), sizeof(ChunkDescriptor));
+        if (m_source_data.rdstate() != std::ios_base::goodbit)
             break;
+
+        const std::streampos riff_payload_begin = m_source_data.tellg();
+        const std::streampos riff_payload_end   = riff_payload_begin + std::streampos(riff_descriptor.size);
+
+        uint32_t riff_type = 0;
+        m_source_data.read(reinterpret_cast<char*>(&riff_type), 4);
+        if (m_source_data.rdstate() != std::ios_base::goodbit)
+            break;
+
+        if (riff_4cc == riff_descriptor.fourcc || wave_4cc == riff_type)
+        {
+            if (FAILED(ReadWafeRiff(riff_payload_begin, riff_payload_end, m_wave_riff)));
+            {
+                m_source_data.seekg(riff_payload_end);
+                continue;
+            }
+        }
+        else
+        {
+            m_source_data.seekg(riff_descriptor.size - 4, std::ios_base::cur); // -4 means except 4 bytes of the riff type FOURCC
         }
     }
 
-    return false;
+    return (bool)m_wave_riff;
+}
+
+HRESULT 
+MyAudioSourceImpl::ReadWafeRiff(const std::streampos& begin, const std::streampos& end, std::unique_ptr<WaveRiff>& wave_riff)
+{
+    HRESULT hr = S_OK;
+    std::streampos tmp = begin;
+    wave_riff.reset();
+    
+    std::unique_ptr<WaveRiff> riff(new WaveRiff);
+
+    if (m_source_data.tellg() != begin)
+        m_source_data.seekg(begin);
+
+    uint32_t type = 0;
+    m_source_data.read(reinterpret_cast<char*>(&type), 4);
+    tmp = m_source_data.tellg();
+
+    if (m_source_data.rdstate() & std::ios_base::failbit || wave_4cc != type) 
+        return E_FAIL;
+
+    while(m_source_data.tellg() < end)
+    {
+        ChunkDescriptor chunk_descriptor{ 0 };
+        m_source_data.read(reinterpret_cast<char*>(&chunk_descriptor), sizeof(ChunkDescriptor));
+        tmp = m_source_data.tellg();
+        if (m_source_data.rdstate() & std::ios_base::failbit)
+            return E_FAIL;
+
+        if (chunk_descriptor.fourcc == fmt_4cc)
+        {
+            hr = ReadFMTChunk(m_source_data.tellg(), chunk_descriptor, riff->format);
+            if (FAILED(hr))
+                return hr;
+        }
+        else if (chunk_descriptor.fourcc == data_4cc)
+        {
+            hr = ReadDataChunk(m_source_data.tellg(), chunk_descriptor, riff->data);
+            if (FAILED(hr))
+                return hr;
+        }
+        else
+        {
+            //m_source_data.seekg(chunk_descriptor.size, std::ios_base::cur); // skip unexpected chunks
+            return E_FAIL; // we are expecting only 'fmt ' chunk followed by 'data' chunk
+        }
+    }
+    
+    riff.swap(wave_riff);
+    
+    return S_OK;
+}
+
+HRESULT 
+MyAudioSourceImpl::ReadFMTChunk(const std::streampos& begin, const ChunkDescriptor& chunk_descr, std::unique_ptr<FmtChunk>& fmt_chunk)
+{
+    fmt_chunk.reset();
+
+    if (chunk_descr.size != 16 && chunk_descr.size != 18 && chunk_descr.size != 40)
+        return E_INVALIDARG;
+
+    std::unique_ptr<FmtChunk> fmt(new FmtChunk);
+    ZeroMemory(fmt.get(), sizeof(FmtChunk));
+
+    if (chunk_descr.size == 16)
+        m_source_data.read(reinterpret_cast<char*>(static_cast<FmtChunk16*>(fmt.get())), sizeof(FmtChunk16));
+    else if (chunk_descr.size == 18)
+        m_source_data.read(reinterpret_cast<char*>(static_cast<FmtChunk18*>(fmt.get())), sizeof(FmtChunk18));
+    else if (chunk_descr.size == 40)
+        m_source_data.read(reinterpret_cast<char*>(static_cast<FmtChunk40*>(fmt.get())), sizeof(FmtChunk40));
+
+    if ((begin + std::streampos(chunk_descr.size)) != m_source_data.tellg())
+        return E_FAIL;
+
+    fmt.swap(fmt_chunk);
+
+    return S_OK;
+}
+
+HRESULT 
+MyAudioSourceImpl::ReadDataChunk(const std::streampos& begin, const ChunkDescriptor& chunk_descr, std::unique_ptr<DataChunk>& data_chunk)
+{
+    data_chunk.reset();
+    std::unique_ptr<DataChunk> chunk(new DataChunk);
+    chunk->chunk_begin = begin;
+    chunk->chunk_end = begin + std::streampos(chunk_descr.size);
+
+    m_source_data.seekg(chunk->chunk_end);
+
+    chunk.swap(data_chunk);
+
+    return S_OK;
 }
 
 MyAudioSourceImpl::~MyAudioSourceImpl()
--- DMOAudioPlayer/RenderStream.h
+++ DMOAudioPlayer/RenderStream.h
@@ -1,6 +1,75 @@
-#ifndef __RENDER_STREAM_H__
+﻿#ifndef __RENDER_STREAM_H__
 #define __RENDER_STREAM_H__
 #pragma once
+
+/*
+<WAVE-form> → RIFF('WAVE'
+                   <fmt-ck>            // Format
+                   [<fact-ck>]         // Fact chunk
+                   [<cue-ck>]          // Cue points
+                   [<playlist-ck>]     // Playlist
+                   [<assoc-data-list>] // Associated data list
+                   <wave-data> )       // Wave data
+*/
+
+struct ChunkDescriptor
+{
+    uint32_t fourcc;
+    uint32_t size;
+};
+
+struct FmtChunk16
+{
+    uint16_t wFormatTag;
+    uint16_t nChannels;
+    uint32_t nSamplesPerSecond;
+    uint32_t nAvgBytesPerSecond;
+    uint16_t nBlockAlign;
+    uint16_t wBitsPerSecond;
+};
+
+struct DataChunk
+{
+    std::streampos                          chunk_begin = 0;
+    std::streampos                          chunk_end = 0;
+};
+
+struct FmtChunk18
+    : FmtChunk16
+{
+    uint16_t cbSize;
+};
+
+typedef
+struct FmtChunk40
+    : FmtChunk18
+{
+    uint16_t wValidBitsPerSample;
+    uint32_t dwChannelMask;
+    GUID     SubFormat;
+} FmtChunk;
+
+struct CueChunk
+{
+
+};
+
+struct PlaylistChunk
+{
+
+};
+
+struct AssocDataListChunk
+{
+
+};
+
+struct WaveRiff
+{
+    std::unique_ptr<FmtChunk>               format;
+    std::unique_ptr<DataChunk>              data;
+};
+
 
 struct MyAudioSource
 {
@@ -8,6 +77,7 @@ struct MyAudioSource
 
     virtual HRESULT GetFormat(std::unique_ptr<WAVEFORMATEX>& pwfx) = 0;
     virtual HRESULT LoadData(UINT32 bufferFrameCount, BYTE* pData, DWORD* pFlags) = 0;
+
 };
 
 class MyAudioSourceImpl
@@ -26,9 +96,15 @@ protected:
     virtual HRESULT GetFormat(std::unique_ptr<WAVEFORMATEX>& pwfx) override;
     virtual HRESULT LoadData(UINT32 bufferFrameCount, BYTE* pData, DWORD* pFlags) override;
 
+    HRESULT ReadWafeRiff(const std::streampos& begin, const std::streampos& end, std::unique_ptr<WaveRiff>& wave_riff);
+    HRESULT ReadFMTChunk(const std::streampos& begin, const ChunkDescriptor& chunk_descr, std::unique_ptr<FmtChunk>& fmt_chunk);
+    HRESULT ReadDataChunk(const std::streampos& begin, const ChunkDescriptor& chunk_descr, std::unique_ptr<DataChunk>& data_chunk);
+
 protected:
     std::ifstream  m_source_data;
     std::streamoff m_file_size;
+
+    std::unique_ptr<WaveRiff> m_wave_riff;
 
     WAVEFORMATEX  m_wfx{ WAVE_FORMAT_PCM, 2, 48000, 192000, 4, 16, 0 };
 //    WAVEFORMATEX  m_wfx{ WAVE_FORMAT_PCM, 2, 48000, 384000, 8, 32, 0 };
